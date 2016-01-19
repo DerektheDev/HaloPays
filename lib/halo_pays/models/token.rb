ce5dd558ce4f2ace3a42f2909b82c35d1fe1dfdd
Derek Montgomery <montgomerygraphics@gmail.com>
2016-01-18 19:49:50 -0600

    Token creation and activation now works

--- lib/halo_pays/models/token.rb
+++ lib/halo_pays/models/token.rb
@@ -4,23 +4,24 @@ module HaloPays
     class << self
       def create_for_ach account_number
         token_payload = {
-          test: Rails.env.development?,
+          test: true,
           pay_type: 'ACH',
           ach_account: account_number
         }.to_json
 
         response = HaloPays.token_connection.post '/tokens/', token_payload
-
         response.body
       end
 
       def create_for_card card_number
-        response = HaloPays.connection.post '/tokens/', {
-          test: Rails.env.development?,
+        token_payload = {
+          test: true,
           pay_type: 'CARD',
           card_number: card_number
         }.to_json
-        JSON.parse response.body
+
+        response = HaloPays.token_connection.post '/tokens/', token_payload
+        response.body
       end
 
       def view token
@@ -35,12 +36,19 @@ module HaloPays
 
       def activate opts
 
+        case opts[:payment][:pay_type]
+        when 'CARD'
+          status, amount = 'AUTHORIZED', 0
+        when 'ACH'
+          status, amount = 'CAPTURED', 100 # TODO: HP will fix this to accept 0 on both
+        end
+
         activate_payload = {
           test:            true,
           trans_type:      'DONATION',
-          status:          'CAPTURED',
+          status:          status,
           order_id:        'some-order-id',
-          amount:          000,
+          amount:          amount,
           order_source:    'ONLINE',
           remote_ip:       opts[:remote_ip],
           description:     'Good Cents Account Activation ($0.00)',
@@ -49,10 +57,9 @@ module HaloPays
           billing_contact: opts[:billing_info]
         }.to_json
 
-        binding.pry
+        response = HaloPays.connection.post "/merchants/#{opts[:merchant_id]}/transactions/", activate_payload
 
-        # response = HaloPays.connection.post "/merchants/#{opts[:merchant_id]}/transactions/", activate_payload
-        # response.body
+        response.body
       end
 
       def delete token
