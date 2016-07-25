89e3bc8197025ee452194b4f378df28a6e734f87
Derek Montgomery <montgomerygraphics@gmail.com>
2016-07-25 16:46:45 -0500

    amount

--- lib/halo_pays/models/transaction.rb
+++ lib/halo_pays/models/transaction.rb
@@ -3,19 +3,19 @@ module HaloPays
     class << self
 
       def post opts
-        case opts[:payment][:pay_type]
-        when 'CARD'
-          status, amount = 'AUTHORIZED', 0
-        when 'ACH'
-          status, amount = 'CAPTURED', 100 # TODO: HP will fix this to accept 0 on both
+        status = case opts[:payment][:pay_type]
+        when 'CARD' then 'AUTHORIZED'
+        when 'ACH' then 'CAPTURED'
         end
+
+        amount = opts[:test] ? 0 : opts[:payment][:amount]
 
         payment_payload = {
           test:            opts[:test],
           trans_type:      'DONATION',
           status:          status,
           order_id:        opts[:order_id],
-          amount:          amount,
+          amount:          opts[:amount].to_i,
           order_source:    'ONLINE',
           remote_ip:       opts[:remote_ip],
           description:     'Good Cents',
